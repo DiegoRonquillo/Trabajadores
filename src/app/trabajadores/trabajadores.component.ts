53ee0e5d391fd41c44253a5f554f94765d75d91a
DiegoRonquillo <rafael81997@gmail.com>
2023-08-06 19:55:51 -0600

    Se suben archivos del proyecto principal

---
+++ src/app/trabajadores/trabajadores.component.ts
@@ -0,0 +1,28 @@
+import { Component , OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-trabajadores',
+  templateUrl: './trabajadores.component.html',
+  styleUrls: ['./trabajadores.component.css']
+})
+export class TrabajadoresComponent implements OnInit{
+  constructor(){}
+
+  ngOnInit(): void{
+
+  }
+
+  numero:number = 1;
+
+  increment(){
+    this.numero = this.numero + 1;
+  }
+
+  decrement(){
+    this.numero = this.numero - 1;
+  }
+  /*datos = {
+    nombre: "Diego",
+    apellido: "Ronquillo"
+  }*/
+}
